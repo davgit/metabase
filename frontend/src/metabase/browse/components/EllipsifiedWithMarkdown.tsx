8e641ea3f354a6de726c23678c748f444d89cdf6
Raphael Krut-Landau <raphael.kl@gmail.com>
2024-05-09 10:01:47 -0400

    Ellipsify descriptions properly in Browse models

--- frontend/src/metabase/browse/components/EllipsifiedWithMarkdown.tsx
+++ frontend/src/metabase/browse/components/EllipsifiedWithMarkdown.tsx
@@ -1,16 +1,28 @@
 import { Ellipsified } from "metabase/core/components/Ellipsified";
 import Markdown from "metabase/core/components/Markdown";
 
-export const EllipsifiedWithMarkdown = ({ children }: { children: string }) => {
-  return (
-    <Ellipsified
-      tooltip={
-        <Markdown disallowHeading unstyleLinks lineClamp={12}>
-          {children}
-        </Markdown>
-      }
-    >
-      <Markdown disallowHeading>{children.replace(/\s/g, " ")}</Markdown>
-    </Ellipsified>
+export const EllipsifiedWithMarkdown = ({
+  shouldMarkdownifyTooltipTarget = false,
+  shouldMarkdownifyTooltip = true,
+  children,
+}: {
+  /** NOTE: If the tooltip target is markdownified,
+   * useIsTruncated won't know whether it has been truncated */
+  shouldMarkdownifyTooltipTarget?: boolean;
+  shouldMarkdownifyTooltip?: boolean;
+  children: string;
+}) => {
+  const tooltip = shouldMarkdownifyTooltip ? (
+    <Markdown disallowHeading unstyleLinks lineClamp={12}>
+      {children}
+    </Markdown>
+  ) : (
+    children
   );
+  const tooltipTarget = shouldMarkdownifyTooltipTarget ? (
+    <Markdown disallowHeading>{children}</Markdown>
+  ) : (
+    children
+  );
+  return <Ellipsified tooltip={tooltip}>{tooltipTarget}</Ellipsified>;
 };
